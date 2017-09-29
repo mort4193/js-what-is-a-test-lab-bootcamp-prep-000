2efea3a0bfb53f7516684f69ba3113a48499ae4f
jonathan Morton <jmorton4193@gmail.com>
2017-09-29 02:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // write your code below!
 var name = 'Susan'
 var height = parseInt(38)
-var message = '${name} is ${height} inches tall'
-
+var message = '"'name'" is "'height'" inches tall'
 module.exports = {name, height, message}
